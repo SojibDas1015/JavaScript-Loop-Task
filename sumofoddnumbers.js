d188c0fb163ac253cd50bd13260c2a2c5514d8db
Sojib Das <sojibdas1015@gmail.com>
2025-12-13 22:51:40 +0600

    sum of odd number and sum of even number using loop and if else

---
+++ sumofoddnumbers.js
@@ -0,0 +1,21 @@
+// odd number
+let oddsum = 0;
+for(let i=91; i<=129; i++)
+{
+    if(i%2 != 0)
+    {
+        oddsum +=i;
+    }
+}
+console.log(oddsum);
+
+// even number
+let evensum = 0;
+for(let i=51; i<=85; i++)
+{
+    if(i%2 == 0)
+    {
+        evensum += i;
+    }
+}
+console.log(evensum);
