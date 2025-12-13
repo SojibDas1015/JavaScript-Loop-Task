66f4d719d49473ed69ca0454c4c5c52c732718c5
Sojib Das <sojibdas1015@gmail.com>
2025-12-13 22:44:52 +0600

    odd number even number using for loop and if else conditional operations

---
+++ odd-even.js
@@ -0,0 +1,17 @@
+// odd number
+for(let i=61; i<=100; i++)
+{
+    if(i%2 != 0)
+    {
+        console.log(i);
+    }
+}
+
+// even number
+for(let i=78; i<=98; i++)
+{
+    if(i%2 == 0)
+    {
+        console.log(i);
+    }
+}
