0f8ccb133ad994b4ac354cf3ccb023a7857bd3ad
Sojib Das <sojibdas1015@gmail.com>
2025-12-17 21:10:12 +0600

    odd number and even number using while loop and condistional statement

--- commitmentWhile.js
+++ commitmentWhile.js
@@ -1,4 +1,5 @@
-for(let i=1; i<= 60; i++)
-{
+let i= 1;
+while (i <= 60) {
     console.log("I will invest at least 6 hrs every single day for next 60 days!");
+    i++;
 }
---
+++ oddevenWhile.js
@@ -0,0 +1,17 @@
+// odd number
+let i = 61;
+while (i <= 100) {
+    if (i % 2 != 0) {
+        console.log(i);
+    }
+    i++;
+}
+
+// even number
+let i = 78;
+while (i <= 98) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+    i++;
+}
